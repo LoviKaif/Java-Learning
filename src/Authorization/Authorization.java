c194c1f74db9f610c54adfb009a4655727159501
LoviKaif <vamparni@gmail.com>
2025-08-13 13:09:23 +0300

    changes in Java

---
+++ src/Authorization/Authorization.java
@@ -0,0 +1,12 @@
+package Authorization;
+
+public class Authorization {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int password;
+        password = 1234;
+
+        System.out.println(password);
+    }
+}
