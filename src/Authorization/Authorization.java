ec432145741dea3a5de191787678f9a1e928d1c7
LoviKaif <vamparni@gmail.com>
2025-08-13 13:47:11 +0300

    Finding a whole percent of a whole number

--- src/Authorization/Authorization.java
+++ src/Authorization/Authorization.java
@@ -1,12 +1,22 @@
 package Authorization;
 
+import java.util.Scanner;
+
 public class Authorization {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input you name: ");
+        String userName = scan.nextLine();
 
-        int password;
-        password = 1234;
+        System.out.println("Hallo " + userName + ",what number do you want to find the percentage of?");
 
-        System.out.println(password);
+        int userNum = scan.nextInt();
+
+        System.out.println("What percentage do you want to find of this number?");
+        int userProcent = scan.nextInt();
+
+        int procent = (userNum / 100) * userProcent;
+
+        System.out.println("Percentage of your number:" + procent);
     }
 }
